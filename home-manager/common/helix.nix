483379b7d43cd995fdedf7c0cbebd0ad8c454178
drac555 <evanmorrison25@gmail.com>
2026-02-12 07:01:16 -0600

    Lock update

--- home-manager/common/helix.nix
+++ home-manager/common/helix.nix
@@ -1,19 +1,30 @@
-{pkgs, ...}:
+{ pkgs, ... }:
 {
-	programs.helix = {
-		enable = true;
-		defaultEditor = true;
-		settings = {
-			editor.cursor-shape = {
-				normal = "block";
-				insert = "bar";
-				select = "underline";
-			};
-		};
-		languages.language = [{
-			name = "nix";
-			auto-format = true;
-			formatter.command = "${pkgs.nixfmt}/bin/nixfmt";
-		}];
-	};
+  programs.helix = {
+    enable = true;
+    defaultEditor = true;
+    settings = {
+      editor.cursor-shape = {
+        normal = "block";
+        insert = "bar";
+        select = "underline";
+      };
+      editor.lsp = {
+        enable = true;
+      };
+    };
+    languages.language = [
+      {
+        name = "nix";
+        auto-format = true;
+        formatter.command = "${pkgs.nixfmt}/bin/nixfmt";
+      }
+    ];
+  };
+
+  home.packages = with pkgs; [
+    nixd
+    clang
+    clang-tools
+  ];
 }
--- home-manager/common/zsh.nix
+++ home-manager/common/zsh.nix
@@ -6,6 +6,15 @@
     autosuggestion.enable = true;
     syntaxHighlighting.enable = true;
 
+    oh-my-zsh = {
+      enable = true;
+      plugins = [
+        "git"
+        "thefuck"
+      ];
+
+    };
+
     shellAliases = {
       ll = "ls -l";
       rebuild = "sudo nixos-rebuild switch";
